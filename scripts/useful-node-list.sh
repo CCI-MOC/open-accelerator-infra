529da9b6c68745ce26c223d929bbdc94a26f262b
Lars Kellogg-Stedman <lars@redhat.com>
2026-08-11 10:17:47 -0400

    Include maintenance state in useful-node-list.sh output

--- scripts/useful-node-list.sh
+++ scripts/useful-node-list.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec openstack baremetal node list --long -c name -c uuid -c provision_state -c instance_uuid -c resource_class -c power_state
+exec openstack baremetal node list --long -c name -c uuid -c provision_state -c instance_uuid -c resource_class -c power_state -c maintenance
